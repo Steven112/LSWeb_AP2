e287a99d74d00fc74b7bb472e5c119dcdd7e4636
Steven112 <54567861+Steven112@users.noreply.github.com>
2020-07-20 21:26:02 -0400

    Solucion de error de nivel de acceso en Login

--- LibraryServicesWeb_AP2/Pages/Login/Login.cshtml.cs
+++ LibraryServicesWeb_AP2/Pages/Login/Login.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using LibraryServicesWeb_AP2.BLL;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -13,45 +14,84 @@ namespace LibraryServicesWeb_AP2.Pages.Login
     public class LoginModel : PageModel
     {
         public string ReturnUrl { get; set; }
+
         public async Task<IActionResult>
+
             OnGetAsync(string paramUsername, string paramPassword)
+
         {
+
             string returnUrl = Url.Content("~/");
+
             try
+
             {
-                // Clear the existing external cookie
+
+
                 await HttpContext
+
                     .SignOutAsync(
+
                     CookieAuthenticationDefaults.AuthenticationScheme);
+
             }
+
             catch { }
-            // *** !!! This is where you would validate the user !!! ***
-            // In this example we just log the user in
-            // (Always log the user in for this demo)
-            var claims = new List<Claim>
+
+
+            if (UsuariosBLL.InicioSesion(paramUsername, paramPassword))
             {
+
+
+
+                var claims = new List<Claim>
+
+            {
+
                 new Claim(ClaimTypes.Name, paramUsername),
-                new Claim(ClaimTypes.Role, "Administrator"), 
-                new Claim(ClaimTypes.Role, "User"),
+
+                new Claim(ClaimTypes.Role, "Administrador"),
+                new Claim(ClaimTypes.Role, "Usuario"),
+
 
             };
-            var claimsIdentity = new ClaimsIdentity(
-                claims, CookieAuthenticationDefaults.AuthenticationScheme);
-            var authProperties = new AuthenticationProperties
-            {
-                IsPersistent = true,
-                RedirectUri = this.Request.Host.Value
-            };
-            try
-            {
-                await HttpContext.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(claimsIdentity),
-                authProperties);
-            }
-            catch (Exception ex)
-            {
-                string error = ex.Message;
+
+                var claimsIdentity = new ClaimsIdentity(
+
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+
+                {
+
+                    IsPersistent = true,
+
+                    RedirectUri = this.Request.Host.Value
+
+                };
+
+                try
+
+                {
+
+                    await HttpContext.SignInAsync(
+
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+
+                    new ClaimsPrincipal(claimsIdentity),
+
+                    authProperties);
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                    string error = ex.Message;
+
+                }
+
             }
             return LocalRedirect(returnUrl);
         }
